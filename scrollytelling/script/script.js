5f1de7465a829b2df0dbcb2c3c9cc7644a8c4524
73Gabriel <gabriellolclerval@gmail.com>
2023-05-08 12:05:24 -0400

    .

--- scrollytelling/script/script.js
+++ scrollytelling/script/script.js
@@ -1,19 +1,32 @@
 import { gsap } from "gsap";
 
-window.addEventListener("scroll", function() {
-
-});
-
-
+window.addEventListener("scroll", function () {});
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.fromTo(
+  ".maison",
+  {
+    x: "0",
+    y: "70vh",
+  },
+  {
+    scrollTrigger: {
+      trigger: "#chapitre1",
+      pin: true,
+      scrub: true,
+    },
+    x:
+      document.querySelector(".chapitre").offsetWidth -
+      document.querySelector(".maison").offsetWidth,
+    y: "40vh",
+  }
+);
 
 ScrollTrigger.create({
-    trigger: ".scroll-story__header",
-    start: "top top",
-    end: "bottom",
+  trigger: ".scroll-story__header",
+  start: "top top",
+  end: "bottom",
 });
